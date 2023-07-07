f9407a867ae39adb72a4d2a318fe9db48690acd3
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-07-07 08:59:42 -0500

    feat(Actividad Gbp - Node Js): :sparkles: EndPoint add productos
    Se agrega endpoint post en el archivo productos con el fin que se permita la creacion de productos

--- routers/productos.js
+++ routers/productos.js
@@ -4,6 +4,7 @@ import {Router} from "express";
 const storageProductos = Router();
 
 dotenv.config();
+
 let con = undefined;
 
 storageProductos.use((req,res,next)=>{
@@ -17,11 +18,36 @@ storageProductos.get("/",(req,res)=>{
         /*sql*/`SELECT p.nombre AS Nombre, SUM(i.cantidad) AS Total 
 FROM productos AS p
 JOIN inventarios AS i ON i.id_producto = p.id
-GROUP BY p.nombre`, 
+GROUP BY p.nombre ORDER BY Total DESC`, 
         (err, data, fil)=>{
             res.send(JSON.parse(JSON.stringify(data)));
         }
     )
+})
+
+storageProductos.post("/",(req,res)=>{
+    let {nombre, descripcion, estado, created_by, created_at, cantidad} = req.query;
+    const sql = `INSERT INTO productos(nombre, descripcion, estado, created_by, created_at) VALUES (?,?,?,?,?)`;
+    const data = [nombre, descripcion, estado, created_by, created_at];
+    con.query(sql,data, (err, result)=>{
+        if (err) {
+            console.error("Error al agregar datos:", err);
+            res.status(500).json({ error: "Error al agregar datos" });
+          } else {
+            let id_producto=result.insertId;
+            const sql2 = `INSERT INTO inventarios(id_producto, cantidad) VALUES (?,?)`;
+            const data2 = [id_producto, cantidad];
+            con.query(sql2,data2, (err, result)=>{
+                if (err) {
+                    console.error("Error al agregar a la tabla inventarios:", err);
+                    res.status(500).json({ error: "Error al agregar datos a inventario" });
+                } else {
+                    console.log("Datos agregados correctamente");
+                    res.status(200).json({ message: "Datos agregados correctamente" });
+                }
+            })
+        }
+    })
 })
 
 export default storageProductos;
